6bf6ae909b615402bb5033f9a19c4786482b7ba5
Eduardo Velez Santiago <eddvelez@outlook.com>
2015-11-14 04:42:46 -0600

    Add Scene/Script: Character Selection

---
+++ Unity Project/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void characterSelection(int faction) {
+        PlayerPrefs.SetInt("FACTION", faction);
+        PlayerPrefs.SetFloat("MELEE",1F);
+        PlayerPrefs.SetFloat("MAGIC", 1F);
+        PlayerPrefs.SetFloat("DEFENSE", 1F);
+
+    }
+}
---
+++ Unity Project/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsManager : MonoBehaviour {
+        
+    //Base stats
+    private float melee;
+    private float magic;
+    private float defense;
+    //On level up pair
+    public float meleePair;
+    public float magicPair;
+    public float defensePair;
+    //On level up non
+    public float meleeNon;
+    public float magicNon;
+    public float defenseNon;
+    //Character Level
+    public float characterLevel;
+    // Use this for initialization
+    void Start () {
+        melee = PlayerPrefs.GetFloat("MELEE");
+        magic = PlayerPrefs.GetFloat("MAGIC");
+        defense = PlayerPrefs.GetFloat("DEFENSE");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void CalculateStat() {
+    }
+}
