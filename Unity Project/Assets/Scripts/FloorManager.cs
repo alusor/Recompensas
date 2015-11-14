2ee7ce0500f6b3d681b6bc95382d7a06ca388e21
Ulisesnama <ulises.nama@gmail.com>
2015-11-14 06:19:58 -0600

    Floor Manager

---
+++ Unity Project/Assets/Scripts/FloorManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorManager : MonoBehaviour {
+
+    public Sprite[] floorTemplates;
+    private GameObject[] floors;
+
+    void Start()
+    {
+        floors = GameObject.FindGameObjectsWithTag("floor");
+        foreach (GameObject obj in floors)
+        {
+            int rand = Random.Range(0, floorTemplates.Length);
+            obj.GetComponent<SpriteRenderer>().sprite = floorTemplates[rand];
+        }
+
+    }
+}
